b1cb41a7e4701689e5f0fa659cc5672185239373
Saúl Báez <ss.baez@comunidad.unam.mx>
2018-03-31 03:58:36 -0600

    Initial commit

---
+++ Ejercicio7.17/src/com/ssbaez/ejercicio717/TiroDados.java
@@ -0,0 +1,39 @@
+package com.ssbaez.ejercicio717;
+
+import java.util.Random;
+
+public class TiroDados {
+	
+	private static Random numAleatorio = new Random();
+	
+	public static void main(String[] args) {
+		
+		int[] matrizTiros = new int[36000];
+		
+		for(int i = 0; i < matrizTiros.length; i++) {
+			matrizTiros[i] = tirarDados();
+		}
+		
+		int[] frecuencia = new int[13];
+		
+		for(int i = 0; i < matrizTiros.length; i++) {
+			++frecuencia[ matrizTiros[i] ];
+		}
+		
+		System.out.println("FRECUENCIAS:\n");
+		
+		for(int i = 2; i < frecuencia.length; i++) {
+			System.out.printf("%2d: %,d%n", i, frecuencia[i]);
+		}
+		
+	}
+	
+	public static int tirarDados() {
+		
+		int dado1 = 1 + numAleatorio.nextInt(6);
+		int dado2 = 1 + numAleatorio.nextInt(6);
+		
+		return dado1 + dado2;
+	}
+
+}
